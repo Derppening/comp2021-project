Derppening/comp2021-project

commit b0da994ab8f9356f2e36f17f39ec9e9d2e26212b

Documentation Pass 3: Complete all documentation
Minor cleanup also included

frame/base.pm
@@ -13,7 +13,16 @@ use lib qw(..);
 use reader::import;
 use reader::readInput;
 
+# Displays the welcoming text
+#
+# Note: In frame::base::scene(), this method is reused to display help text
+#
+# arg1: (Optional:) Only display help text
+#
 sub welcome {
+  if (scalar(@_) != 0 || scalar(@_) != 1) {
+    util::DieArgs("frame::base::welcome()", "<2", scalar(@_));
+  }
   if (!exists $_[0]) {
     print "Welcome to the game. ";
   } else {
@@ -32,18 +41,13 @@ sub welcome {
   system("clear");
 }
 
-sub isAns{
-  #$numAlt,$playerAns
-  # if (looks_like_number($_[1])){
-  for (my $i = 1; $i <= $_[0]; $i++) {
-    if ($i == $_[1]) {
-      return 1;
-    }
-  }
-  # }
-  return 0;
-}
-
+# Displays a scene, reads user input, and invokes the next scene
+#
+# arg1: (Reference to) file contents array
+# arg2: (Reference to) plot header hash
+# arg3: (Reference to) characters hash
+# arg4: (Reference to) data hash
+#
 sub scene {
   if (scalar(@_) != 4) {
     util::DieArgs("frame::base::scene()", 4, scalar(@_));
@@ -56,24 +60,25 @@ sub scene {
   
   my $playerAns = "";
   my %opinion;
-  my $numAlt = 1; #Should be reset later on
-  my $sceneInd = 1; #Should be reset later on
-  my $sceneText = "\n";
+  my $sceneInd = 1;
 
   $sceneInd = int($data{'scene'}) if exists $data{'scene'};
   
   while(lc($playerAns) ne "q"){
     system("clear");
     
+    # read and display the plot text
     util::PrintAtPos('m', 2, "");
     util::SetCursorPos('l', 4);
     %opinion = reader::readInput(\@file, \%section, \%char, $sceneInd);
-
+    
     util::SetCursorPos('l', "bb");
     util::ClearLine();
     util::SetCursorPos('l', "bb");
     print ": ";
     chomp($playerAns = <STDIN>);
+    
+    # read the option...
     if ($playerAns eq "_save") {
       $data{'scene'} = $sceneInd;
       if (frame::gameHandler::SaveGame(\%data) == 0) {
@@ -88,8 +93,9 @@ sub scene {
       welcome(0);
       next;
     }
-    $sceneInd = reader::goPlot(\%opinion, $playerAns);
     
+    # not a command: analyze using goPlot(...)
+    $sceneInd = reader::goPlot(\%opinion, $playerAns);
     while($sceneInd eq -1) {
       util::SetCursorPos('l', 'b');
       print "$playerAns: Invalid option";
@@ -106,15 +112,16 @@ sub scene {
       $sceneInd = reader::goPlot(\%opinion, $playerAns);
     }
     
+    # user responded with 'q'
     if ($sceneInd eq 0) {
       print "Hope you enjoy the game. Bye :D";
       sleep(2);
       system("clear");
       last;
     }
+    
     system("clear");
   }
 }
 
-# All Perl modules need to end with a true value.
 1;

main.pl
@@ -25,7 +25,7 @@ use util::terminal;
 # Displays the version text and quits
 #
 sub VersionText {
-  print "Bob 1.0.0-prelease\n";
+  print "Bob 1.0.0-prelease.1\n";
   exit;
 }
 

reader/sceneselector.pm
@@ -14,56 +14,60 @@ use lib qw(..);
 use frame::gameHandler;
 use util::die;
 
-# Choose to read which files
+# Choose which scene/savefile to load from
 #
-# return: filename
+# return: Hash of load data
 #
 sub SceneSelector {
   my $resp = "";
   my $numoffiles = 0;
 
+  # read '*.txt' in './', except 'art.txt'
   opendir my $dir, "./" or die "Cannot open directory: $!";
   my @files = grep(/[^art]\.txt$/, readdir($dir));
   closedir $dir;
 
   system("clear");
 
+  # print gui
   util::PrintAtPos('m', 't', "=== Scene Selection ===");
-
   util::SetCursorPos('l', 2);
   print "Start a new story from an existing plot: \n\n";
-
   foreach my $file (@files) {
     print ++$numoffiles . ". " . $file . "\n";
   }
-
-  my @saves = frame::gameHandler::GetSaves();
-
-  if (@saves) {
+  if (frame::gameHandler::GetSaves()) {
     print "\nOr load from a savefile. (type 'load') \n";
   }
-
   print "\n(selection): ";
 
   $resp = <STDIN>;
   chomp($resp);
 
   my %data = ();
-
+  
   if ($resp eq "load") {
+    # load from an existing save
     %data = frame::gameHandler::LoadGame();
   } elsif ($resp =~ /[\D\s]+/ or $resp eq "") {
+    # invalid input: contains non-numeric/input is blank
     print "$resp: Not a valid file number\n";
     sleep(2);
     util::ClearLine();
+    
+    # recursively call this function until we have a valid input
     return SceneSelector();
   } elsif (int($resp) > $numoffiles || int($resp) < 1) {
+    # invalid input: out-of-bounds
     print "$resp: Not a valid file number\n";
     sleep(2);
     util::ClearLine();
+    
+    # recursively call this function until we have a valid input
     return SceneSelector();
   }
 
+  # %data is not blank implies a savefile is already loaded
   if (!%data) {
     print "=========================================\n";
     $data{'scenefile'} = $files[int($resp - 1)];

reader/readInput.pm
@@ -16,9 +16,9 @@ use reader::import;
 # Read input from users and return options hash table
 #
 # arg1: (Reference of) File content array
-# arg2: Hash of section
-# arg3: Hash of characters
-# arg4: Current scene id
+# arg2: (Reference of) Hash of section
+# arg3: (Reference of) Hash of characters
+# arg4: (Reference of) Current scene id
 #
 # return: Hash of option in current plot
 #
@@ -35,10 +35,11 @@ sub readInput {
 
 # Return the plot number according to the options
 #
-# arg1: Hash of option in current plot
+# arg1: (Reference to) Hash of option in current plot
 # arg2: User answer
 #
 # return: Cooresponding plot number
+#
 sub goPlot {
   my %option = %{$_[0]};
   my $myAns = $_[1];
@@ -49,7 +50,7 @@ sub goPlot {
   } elsif ($myAns eq 'q') {
     $result = 0;
   } else {
-    # The user input is not match with any options in this plot
+    # The user input does not match with any options in this plot
     $result = -1;
   }
   

util/terminal.pm
@@ -37,20 +37,6 @@ sub GetTermSize {
   return @term;
 }
 
-# Sets the size of the current terminal
-#
-# arg1: New x-size of terminal
-# arg2: New y-size of terminal
-#
-sub SetTermSize {
-  if (scalar(@_) != 2) {
-    DieArgs("util::SetTermSize()", 2, scalar(@_));
-  }
-  
-  my @term = GetTerminalSize();
-  SetTerminalSize($_[0], $_[1], $term[2], $term[3]);
-}
-
 # Resets the position of the cursor
 #
 sub ResetCursor {

commit e9bbb29b10d25d76fa8745527f41a5dbf1fccaf4

Bug-Bash Part 8
- Improved look for presentation.txt
- Improved logic in game loop
- Refractor all frame::* packages to frame
- Refractor all $fh to $filecheck if there is no file I/O
- Fixed game-breaking regression from b0da994
- Fixed unable to use '_save' or '_help' after a wrong input
- Fixed crash user deletes a savefile externally while in scene selector

creator/editmenu.pm
@@ -34,7 +34,7 @@ sub EditMenu {
   }
   
   # check if the file exists and is valid, output a message if it's not okay
-  if (!(open(my $fh, '<:encoding(UTF-8)', $filename))) {
+  if (!(open(my $flecheck, '<:encoding(UTF-8)', $filename))) {
     util::PrintAtPos('l', 'b', "Cannot read from $filename: $!");
     sleep(2);
     util::SetCursorPos('l', 'b');

frame/base.pm
@@ -3,7 +3,7 @@
 # Manages the general gameplay.
 #
 
-package frame::base;
+package frame;
 
 use strict;
 use warnings FATAL => 'all';
@@ -20,8 +20,8 @@ use reader::readInput;
 # arg1: (Optional:) Only display help text
 #
 sub welcome {
-  if (scalar(@_) != 0 || scalar(@_) != 1) {
-    util::DieArgs("frame::base::welcome()", "<2", scalar(@_));
+  if (scalar(@_) != 0 && scalar(@_) != 1) {
+    util::DieArgs("frame::welcome()", "<2", scalar(@_));
   }
   if (!exists $_[0]) {
     print "Welcome to the game. ";
@@ -50,7 +50,7 @@ sub welcome {
 #
 sub scene {
   if (scalar(@_) != 4) {
-    util::DieArgs("frame::base::scene()", 4, scalar(@_));
+    util::DieArgs("frame::scene()", 4, scalar(@_));
   }
 
   my @file = @{$_[0]};
@@ -59,7 +59,7 @@ sub scene {
   my %data = %{$_[3]};
   
   my $playerAns = "";
-  my %opinion;
+  my %opinion = ();
   my $sceneInd = 1;
 
   $sceneInd = int($data{'scene'}) if exists $data{'scene'};
@@ -68,11 +68,12 @@ sub scene {
     system("clear");
     
     # read and display the plot text
-    util::PrintAtPos('m', 2, "");
-    util::SetCursorPos('l', 4);
+    util::SetCursorPos('l', 1);
     %opinion = reader::readInput(\@file, \%section, \%char, $sceneInd);
     
-    util::SetCursorPos('l', "bb");
+    util::SetCursorPos('l', 'b');
+    util::ClearLine();
+    util::SetCursorPos('l', 'bb');
     util::ClearLine();
     util::SetCursorPos('l', "bb");
     print ": ";
@@ -81,7 +82,7 @@ sub scene {
     # read the option...
     if ($playerAns eq "_save") {
       $data{'scene'} = $sceneInd;
-      if (frame::gameHandler::SaveGame(\%data) == 0) {
+      if (SaveGame(\%data) == 0) {
         util::SetCursorPos('l', 5);
         util::ClearLine();
         print "Game successfully saved";
@@ -95,30 +96,24 @@ sub scene {
     }
     
     # not a command: analyze using goPlot(...)
-    $sceneInd = reader::goPlot(\%opinion, $playerAns);
-    while($sceneInd eq -1) {
+    my $plot_res = reader::goPlot(\%opinion, $playerAns);
+    if ($plot_res eq -1) {
       util::SetCursorPos('l', 'b');
       print "$playerAns: Invalid option";
       
       sleep(2);
-      
-      util::SetCursorPos('l', 'b');
-      util::ClearLine();
-      util::SetCursorPos('l', 'bb');
-      util::ClearLine();
-      util::SetCursorPos('l', "bb");
-      print ": ";
-      chomp($playerAns = <STDIN>);
-      $sceneInd = reader::goPlot(\%opinion, $playerAns);
+      next;
     }
     
     # user responded with 'q'
-    if ($sceneInd eq 0) {
+    if ($plot_res eq 0) {
       print "Hope you enjoy the game. Bye :D";
       sleep(2);
       system("clear");
       last;
     }
+  
+    $sceneInd = $plot_res;
     
     system("clear");
   }

frame/gameHandler.pm
@@ -3,7 +3,7 @@
 # Manages the save/load feature of the game.
 #
 
-package frame::gameHandler;
+package frame;
 
 use strict;
 use warnings 'FATAL' => 'all';
@@ -19,7 +19,7 @@ use util::die;
 #
 sub SaveGame {
   if (scalar(@_) != 1) {
-    util::DieArgs("frame::gameHandler::SaveGame()", 1, scalar(@_));
+    util::DieArgs("frame::SaveGame()", 1, scalar(@_));
   }
   
   my %data = %{$_[0]};
@@ -52,7 +52,7 @@ sub SaveGame {
   chomp($filename);
   
   # check if the file already exists
-  if (open(my $filecheck, "$filename.sav")) {
+  if (open(my $filecheck, '<:encoding(UTF-8)', "$filename.sav")) {
     util::SetCursorPos('l', 5);
     util::ClearLine();
     print "$filename.sav already exists. Do you want to overwrite it? (y/N) ";
@@ -82,7 +82,7 @@ sub SaveGame {
 #
 sub WriteToFile {
   if (scalar(@_) != 2) {
-    util::DieArgs("frame::gameHandler::WriteToFile()", 2, scalar(@_));
+    util::DieArgs("frame::WriteToFile()", 2, scalar(@_));
   }
   
   my $filename = $_[0];
@@ -104,12 +104,12 @@ sub WriteToFile {
 #
 sub ReadFromFile {
   if (scalar(@_) != 1) {
-    util::DieArgs("frame::gameHandler::ReadFromFile()", 1, scalar(@_));
+    util::DieArgs("frame::ReadFromFile()", 1, scalar(@_));
   }
   
   my $filename = $_[0];
   my @lines = ();
-  if (!open (my $fh, '<:encoding(UTF-8)', $filename)) {
+  if (!open (my $filecheck, '<:encoding(UTF-8)', $filename)) {
     print "Cannot open $filename: $!";
     return @lines;
   }
@@ -127,7 +127,7 @@ sub ReadFromFile {
 #
 sub LoadGame {
   if (scalar(@_) != 0) {
-    util::DieArgs("frame::gameHandler::LoadGame()", 0, scalar(@_));
+    util::DieArgs("frame::LoadGame()", 0, scalar(@_));
   }
   
   system("clear");
@@ -143,10 +143,17 @@ sub LoadGame {
   }
 
   my @lines = ();
+  my @files = GetSaves();
+  
+  if (!@files) {
+    util::PrintAtPos('l', 2, "No saves found.");
+    util::PrintAtPos('l', 4, "Press <ENTER> to continue...");
+    <STDIN>;
+    return %data;
+  }
   
   util::PrintAtPos('l', 2, "Choose a file to load from: ");
   util::SetCursorPos('l', 3);
-  my @files = GetSaves();
   foreach my $file (@files) {
     print "- $file\n";
   }
@@ -189,7 +196,7 @@ sub LoadGame {
 #
 sub GetSaves {
   if (scalar(@_) != 0) {
-    util::DieArgs("frame::gameHandler::ListSaves()", 0, scalar(@_));
+    util::DieArgs("frame::ListSaves()", 0, scalar(@_));
   }
   
   my @saves = ();
@@ -211,7 +218,7 @@ sub GetSaves {
 #
 sub DeleteSave {
   if (scalar(@_) != 1) {
-    util::DieArgs("frame::gameHandler::DeleteSave()", 1, scalar(@_));
+    util::DieArgs("frame::DeleteSave()", 1, scalar(@_));
   }
   
   my $filename = $_[0];

main.pl
@@ -9,8 +9,6 @@
 use strict;
 use warnings FATAL => 'all';
 
-use Data::Dumper;
-
 use lib '.';
 
 use creator::mainmenu;
@@ -25,7 +23,7 @@ use util::terminal;
 # Displays the version text and quits
 #
 sub VersionText {
-  print "Bob 1.0.0-prelease.1\n";
+  print "Bob 1.0.0-prelease.2\n";
   exit;
 }
 
@@ -121,9 +119,9 @@ sub main {
     delete($data{'savefile'});
     sleep(2);
   } else {
-    frame::base::welcome();
+    frame::welcome();
   }
-  frame::base::scene(\@file, \%section, \%chars, \%data);
+  frame::scene(\@file, \%section, \%chars, \%data);
 }
 
 # Program Entry Point

reader/readplot.pm
@@ -11,7 +11,6 @@ use warnings FATAL => 'all';
 use lib qw(..);
 
 use util::die;
-use open ':std', ':encoding(UTF-8)';
 
 # Interprets and outputs the plot to stdout
 #

reader/sceneselector.pm
@@ -36,7 +36,7 @@ sub SceneSelector {
   foreach my $file (@files) {
     print ++$numoffiles . ". " . $file . "\n";
   }
-  if (frame::gameHandler::GetSaves()) {
+  if (frame::GetSaves()) {
     print "\nOr load from a savefile. (type 'load') \n";
   }
   print "\n(selection): ";
@@ -48,7 +48,10 @@ sub SceneSelector {
   
   if ($resp eq "load") {
     # load from an existing save
-    %data = frame::gameHandler::LoadGame();
+    %data = frame::LoadGame();
+    if (!%data) {
+      return SceneSelector();
+    }
   } elsif ($resp =~ /[\D\s]+/ or $resp eq "") {
     # invalid input: contains non-numeric/input is blank
     print "$resp: Not a valid file number\n";